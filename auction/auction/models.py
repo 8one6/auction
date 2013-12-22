f28df266a38f23cf524fb54d2ca8037695ab931e
8one6 <jbrodie@8one6.com>
2013-12-22 18:21:07 -0500

    Fixing CharFields in auction.models and syncing db

--- auction/auction/models.py
+++ auction/auction/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Lot(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     description = models.TextField()
     openforbids = models.BooleanField(default=False)
     votecontrol = models.BooleanField(default=False)
@@ -13,7 +13,7 @@ class Lot(models.Model):
 class Bid(models.Model):
     lot = models.ForeignKey('Lot')
     amount = models.FloatField()
-    biddername = models.CharField()
+    biddername = models.CharField(max_length=200)
     
     def __unicode__(self):
         return u'Bid %'
